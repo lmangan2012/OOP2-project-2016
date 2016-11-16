4f985a53f6f7edec3a0254d86c5a4ad36a85914f
lmangan2012 <lmangan2012@gmail.com>
2016-11-16 12:40:56 +0000

    dvd.java (Version 2)

--- dvd.java
+++ dvd.java
@@ -3,17 +3,34 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class dvd extends JFrame {
-
-    public static void main (String [] args) {
-       	dvd mine = new dvd();
+    // private instance variables
+    JButton oneButton;
+    JLabel welcomeLabel;
+    
+    /** driver */
+    public static void main (String [] args) {  
+		dvd mine = new dvd();
         mine.setVisible(true);
     }
 
     public dvd () {
-        this.setTitle ("DVD Main Menu");
-        this.setSize (500, 500);
-
+        setTitle ("DVD Main Menu (V2)");
+        setSize (500, 500);
         setDefaultCloseOperation( EXIT_ON_CLOSE );
+
+        // get the content pane and set properties
         Container contentPane = getContentPane();
+        contentPane.setLayout(new FlowLayout());
+        
+        JLabel welcomeLabel = new JLabel("Welcome");
+        contentPane.add(welcomeLabel);
+        
+        
+        // construct two buttons
+        oneButton = new JButton("Rent DVD");
+        oneButton.setBounds(150,150,100,50);
+        contentPane.add(oneButton);
+        
+     
     }
 }
