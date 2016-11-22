22c68a8a75f283dc1bc5991fd417a4bd042597e3
lmangan2012 <lmangan2012@gmail.com>
2016-11-22 18:13:23 +0000

    Add files via upload
    dvd.java (Version 5)

--- dvd.java
+++ dvd.java
@@ -4,9 +4,9 @@ import java.awt.event.*;
 
 public class dvd extends JFrame{
     // private instance variables
-    JButton oneButton;
+    JButton oneButton, twoButton;
     JLabel welcomeLabel;
-    JPanel mainMenu;
+    JPanel newPanel,newPanel2;
     JMenu fileMenu;  
     JMenu editMenu;
     JMenuItem item1,item2;
@@ -15,44 +15,40 @@ public class dvd extends JFrame{
 
     /** driver */
     public static void main (String [] args) {  
-		dvd mine = new dvd();
-        mine.setVisible(true);
+		dvd dvd = new dvd();
+        dvd.setVisible(true);
     }
 
     public dvd () {
         setTitle ("DVD Main Menu");
-        setSize (500, 500);
+        setSize (600,600);
         setResizable(false);
         setDefaultCloseOperation( EXIT_ON_CLOSE );
 
         // get the content pane and set properties
         Container contentPane = getContentPane();
-        contentPane.setLayout(new FlowLayout());
-       
+        contentPane.setLayout(new GridBagLayout());
+        
 
         JPanel newPanel = new JPanel();
-        newPanel.setLayout(new BorderLayout());
-        contentPane.add(newPanel);
-        
-  
+        newPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        contentPane.add(newPanel, new GridBagConstraints());
         
         
-        JLabel welcomeLabel = new JLabel("Welcome");
+        newPanel.setPreferredSize(new Dimension(500,200));
+        newPanel.setBackground (Color.PINK);
+                 
+        JLabel welcomeLabel = new JLabel("Welcome to the DVD store. Please" 
+        						+ " select an option from the menu above.");
        	newPanel.add(welcomeLabel);
-       	newPanel.set
-        
-    
-        oneButton = new JButton("Rent DVD");
-        oneButton.setBounds(250,250,100,50);
-        contentPane.add(oneButton);
-        
+               
         fileMenu = new JMenu("File");
         editMenu = new JMenu("Exit");
         
         item1 = new JMenuItem("New"); 
         item2 = new JMenuItem("Open...");
-        
-        fileMenu.add(item1);
+       
+        fileMenu.add(item1); 
         fileMenu.add(item2);
         
         JMenuBar menuBar = new JMenuBar();
