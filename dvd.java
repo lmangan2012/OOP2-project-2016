44897567f9fd557fb75577f2eac70ccf80b70e8c
lmangan2012 <lmangan2012@gmail.com>
2016-11-22 21:19:18 +0000

    Add files via upload
    dvd.java (Version 6)
    dvd_rental_shop.jpg

--- dvd.java
+++ dvd.java
@@ -6,7 +6,7 @@ public class dvd extends JFrame{
     // private instance variables
     JButton oneButton, twoButton;
     JLabel welcomeLabel;
-    JPanel newPanel,newPanel2;
+    JPanel imagePanel,newPanel;
     JMenu fileMenu;  
     JMenu editMenu;
     JMenuItem item1,item2;
@@ -24,11 +24,17 @@ public class dvd extends JFrame{
         setSize (600,600);
         setResizable(false);
         setDefaultCloseOperation( EXIT_ON_CLOSE );
+        
+       
 
         // get the content pane and set properties
         Container contentPane = getContentPane();
         contentPane.setLayout(new GridBagLayout());
         
+        JPanel imagePanel = new JPanel(new FlowLayout());
+        imagePanel.add(new JLabel(new ImageIcon("images/dvd_rental_shop.jpg")));
+        imagePanel.setPreferredSize(new Dimension(500,200));
+        imagePanel.setBackground (Color.PINK);
 
         JPanel newPanel = new JPanel();
         newPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -37,9 +43,13 @@ public class dvd extends JFrame{
         
         newPanel.setPreferredSize(new Dimension(500,200));
         newPanel.setBackground (Color.PINK);
-                 
-        JLabel welcomeLabel = new JLabel("Welcome to the DVD store. Please" 
-        						+ " select an option from the menu above.");
+       
+       
+        JLabel welcomeLabel = new JLabel("<html>Welcome to the Parkway DVD Rental shop. <br/> " +
+        						"Please select an option from the menu above to avail of our many features.</html>");
+        
+        //Code Source: http://stackoverflow.com/questions/7861724/is-there-a-word-wrap-property-for-jlabel
+       	
        	newPanel.add(welcomeLabel);
                
         fileMenu = new JMenu("File");
