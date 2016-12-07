da71a8f35818aead721a1dadb458e6b7666b2eaa
lmangan2012 <lmangan2012@gmail.com>
2016-12-07 23:54:28 +0000

    DVDRentalGUI.java (Version 19)

--- DVDRentalGUI.java
+++ DVDRentalGUI.java
@@ -1,23 +1,30 @@
-import javax.swing.*;
+import javax.swing.*; 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 import java.util.*;
+import java.io.Serializable;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
 
-public class DVDRentalGUI extends JFrame implements ActionListener{
+public class DVDRentalGUI extends JFrame implements Serializable, ActionListener{
     // private instance variables
-    JButton oneButton, twoButton;
+    JButton rentButton;
     JLabel welcomeLabel,headingLabel;
     JPanel headingPanel,mainPanel;
     JMenu customerMenu,dvdMenu,exitMenu;
     JMenuItem item1,item2,item3,item4,item5,item6,item7,exitItem;
     ArrayList <Customer> customerArrayList = new ArrayList <Customer>();
     ArrayList <DVD> DVDArrayList = new ArrayList <DVD>();
+   
     
     String custName, custAddress, title, genre, rating, customer;
     int age, year, count;
     double price;
-    
     
     
 
@@ -57,7 +64,7 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
         
         
         mainPanel.setPreferredSize(new Dimension(500,200));
-        mainPanel.setBackground (Color.YELLOW);
+        mainPanel.setBackground (Color.PINK);
        
         welcomeLabel = new JLabel("<html><p>Welcome to the Parkway DVD Rental store.</p> <br/> " +
         						"<p>Staff can select an option from the menu above to manage the database.</p><br/> " +
@@ -65,12 +72,10 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
         
          //Code Source: http://stackoverflow.com/questions/7861724/is-there-a-word-wrap-property-for-jlabel
         
-        
-        
-        						 
+						 
         JButton rentButton = new JButton();
         rentButton.setText("Rent a DVD");
-
+		rentButton.addActionListener(this);
        
        	
        	mainPanel.add(welcomeLabel);
@@ -80,7 +85,7 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
         
         customerMenu = new JMenu("Manage Customers");       
         dvdMenu = new JMenu("Manage DVDs");
-        exitMenu = new JMenu("Exit");
+        exitMenu = new JMenu("Save and Exit");
         
         
         exitItem = new JMenuItem("Exit System");
@@ -118,12 +123,80 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
         menuBar.add(dvdMenu);
         menuBar.add(exitMenu);
         
+        loadCustomers();
+        loadDVDs();
+     
     }
-
+    
+    	
+   public void loadCustomers()
+   {
+   	 try{
+   	 	 
+    		String fileName = "customers.ser";
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream(fileName));
+			ArrayList <Customer> customerArrayList = (ArrayList <Customer>)is.readObject();
+			
+			/*The following code is just to verify that the customers that had been saved to the file are accessible now
+			//after relaunching the application*/
+			
+			if(customerArrayList!=null)		
+			for (int i = 0; i<customerArrayList.size(); i++){
+            				JOptionPane.showMessageDialog(null, "Name: " + customerArrayList.get(i).getCustName() + "\nAddress: " + customerArrayList.get(i).getCustAddress() + "\nAge: " + customerArrayList.get(i).getCustAge());
+        			}
+					
+					
+					is.close();
+						
+    
+		} catch (FileNotFoundException f) {
+						f.printStackTrace();
+						System.out.println("The file customers.ser has not yet been created");
+				   } catch (IOException f) {
+						f.printStackTrace();
+				   } catch (ClassNotFoundException f) {
+						f.printStackTrace();
+				   }			   
+   }
+   
+   public void loadDVDs()
+   {
+   	 try{
+   	 	 
+    		String fileName = "dvds.ser";
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream(fileName));
+			ArrayList <DVD> DVDArrayList = (ArrayList <DVD>)is.readObject();
+			
+			/*The following code to verify that the DVDs that had been saved to the file are accessible now
+			after relaunching the application*/
+			
+			if(customerArrayList!=null)		
+			for (int i = 0; i<DVDArrayList.size(); i++){
+            				JOptionPane.showMessageDialog(null, "Title: " + DVDArrayList.get(i).getTitle() + "\nAddress: " + DVDArrayList.get(i).getYear() + "\nGenre: " + DVDArrayList.get(i).getGenre() +
+            					 								"\nRating: " + DVDArrayList.get(i).getRating() + "\nPrice: " + DVDArrayList.get(i).getPrice());
+        			}
+					
+					is.close();
+						
+    
+		} catch (FileNotFoundException d) {
+					d.printStackTrace();
+					System.out.println("The file dvds.ser has not yet been created");
+				} catch (IOException d) {
+						d.printStackTrace();
+				   } catch (ClassNotFoundException d) {
+						d.printStackTrace();
+				   }
+				   
+				   
+   }
+   
     public void actionPerformed(ActionEvent e) {
+    	 
+    	
+    	
         String  menuName=e.getActionCommand();
         if (menuName.equals("Add Customer")) {
-        	//while(answer.equals("yes")){
 				custName = JOptionPane.showInputDialog("Please enter the customer's name:");
 				 if (custName == null)
                 {
@@ -153,20 +226,16 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
                 			       
                 					
                 						Customer customer = new Customer(custName,custAddress,age);
-            						
-                						count++;
+                						
+                						customerArrayList.add(customer);
                 						
                 						 
                 						for (int i = 0; i<customerArrayList.size(); i++){
             								JOptionPane.showMessageDialog(null, "Name: " + customerArrayList.get(i).getCustName() + "\nAddress: " + customerArrayList.get(i).getCustAddress() + "\nAge: " + customerArrayList.get(i).getCustAge());
         								}
                 				
-                				
-										    //You can set it up so that when the "Exit" menu item is selected, the Customer and DVD array lists
-										    //are saved to a file on disk
                 						
-                						
-                						JOptionPane.showMessageDialog (null, "The customer " + custName + " has been added to the array-list.", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                						JOptionPane.showMessageDialog (null, "The customer " + custName + " has been added to the database.", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
                 						
                 					}
                 			
@@ -175,31 +244,59 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
                 			
                 			
                  }
-               //answer = JOptionPane.showInputDialog("Do you wish to add another customer?");
 			}
-        //}
+        
 			
 		
-	//if (menuName.equals("Edit Customer")) {
+	if (menuName.equals("Edit Customer")) {
+		 for (int i = 0; i<customerArrayList.size(); i++){
+            customerArrayList.get( i);
+            JOptionPane.showMessageDialog(null, "Name: " + customerArrayList.get(i).getCustName() + "\nAddress: " + customerArrayList.get(i).getCustAddress() + "\nAge: " + customerArrayList.get(i).getCustAge());
+        }
+		
+		String searchCustName = JOptionPane.showInputDialog("Please enter customer name to search for:");
+			for (int i = 0; i<customerArrayList.size(); i=i+1){
+				if(searchCustName.equalsIgnoreCase(customerArrayList.get(i).getCustName())){
+					JOptionPane.showMessageDialog(null, "Name: " + customerArrayList.get(i).getCustName() + "\nAddress: " + customerArrayList.get(i).getCustAddress() + "\nAge: " + customerArrayList.get(i).getCustAge());	
+				}
+            	
+        	}
+		
+		String editCustomer = JOptionPane.showInputDialog("Please enter detail to change(name,address,age):");
 			
+					if (editCustomer.equals("name"))
+                			{
+                				custName = JOptionPane.showInputDialog("Please enter the new name:");
+                    			//customerArrayList.set(0,0);
+                    
+                    			JOptionPane.showMessageDialog (null, "The name has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+                
+                 	if (editCustomer.equals("address"))
+                			{
+                				String newCustAddress = JOptionPane.showInputDialog("Please enter the new address:");
+                    			// customerArrayList.set(0,1);
+                    			JOptionPane.showMessageDialog (null, "The address has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
 			
-           //welcomeLabel.setText("Menu Item '" + menuName + "' is selected.");
-        //} // end if
+					if (editCustomer.equals("age"))
+                			{
+                				String newAgeAsString = JOptionPane.showInputDialog("Please enter the new age:");
+                				int newAge = Integer.parseInt(newAgeAsString);                    			// customerArrayList.set( 2, "New" );
+                    			// customerArrayList.set(0,2);
+                    			JOptionPane.showMessageDialog (null, "The age has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+        } // end if
         
-        //else{
-			//System.exit(0);
-				
-			//}		
-				
-				
-		//if (menuName.equals("Remove Customer")) {
-           //welcomeLabel.setText("Menu Item '" + menuName + "' is selected.");
-        //} // end if
         
-        //else{
-				//System.exit(0);
 				
-			//}
+		if (menuName.equals("Remove Customer")) {
+           		//customerArrayList.remove(customer);
+           
+    
+        } // end if
+        
+        
 			
 			
 			
@@ -247,28 +344,14 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
                 							
                 													else{
          
-            						
-                					//	count++;
-                						
-                						 
-                						//for (int i = 0; i<customerArrayList.size(); i++){
-            								//JOptionPane.showMessageDialog(null, "Name: " + customerArrayList.get(i).getCustName() + "\nAddress: " + customerArrayList.get(i).getCustAddress() + "\nAge: " + customerArrayList.get(i).getCustAge());
-        								//}
                 															DVD dvd = new DVD(title,year,genre,rating,price);
-            						
-                																count++;
+                																DVDArrayList.add(dvd);
                 						
                 						 
                 															for (int i = 0; i<DVDArrayList.size(); i++){
             																		JOptionPane.showMessageDialog(null, "Title: " + DVDArrayList.get(i).getTitle() + "\nYear: " + DVDArrayList.get(i).getYear() + "\nGenre: " + DVDArrayList.get(i).getGenre() + 
             																											"\nRating: " + DVDArrayList.get(i).getRating() + "\nPrice: " + DVDArrayList.get(i).getPrice());
         																			}
-                																				
-                														    //JB - some suggestions
-                														    //Create a DVD object at this point - you'll need a separate DVD class for your project,
-                														    //Add the DVD object to an array list of DVDs
-                														    //You can set it up so that when the "Exit" menu item is selected, the Customer and DVD array lists
-                														    //are saved to a file on disk
                 														    
                 															JOptionPane.showMessageDialog (null, "The DVD " + title + " has been added to the database.", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
                 														}
@@ -282,37 +365,103 @@ public class DVDRentalGUI extends JFrame implements ActionListener{
                  		}
 			
 			
-         	}	
+         	}
+         	
+         	
+         if (menuName.equals("Edit DVD")) {
+         	
+		String editDVD = JOptionPane.showInputDialog("Enter detail to change(title,year,genre,rating,price):");
+			
+					if (editDVD.equals("title"))
+                			{
+                				String newTitle = JOptionPane.showInputDialog("Please enter the new title:");
+                    			 //DVDArrayList.set(0,0);
+                    			JOptionPane.showMessageDialog (null, "The detail has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+                
+                 	if (editDVD.equals("year"))
+                			{
+                				String newYearAsString = JOptionPane.showInputDialog("Please enter the new year:");
+                				int newYear = Integer.parseInt(newYearAsString);
+                    			// DVDArrayList.set(0,1);
+                    			JOptionPane.showMessageDialog (null, "The detail has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+			
+					if (editDVD.equals("genre"))
+                			{
+                				String newGenre = JOptionPane.showInputDialog("Please enter the new genre:");		// customerArrayList.set( 2, "New" );
+                    			// DVDArrayList.set(0,2);
+                    			JOptionPane.showMessageDialog (null, "The detail has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+                			
+                	if (editDVD.equals("rating"))
+                			{
+                				String newRating = JOptionPane.showInputDialog("Please enter the new rating:");
+                    			// DVDArrayList.set(0,3);
+                    			JOptionPane.showMessageDialog (null, "The detail has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+                			
+                	if (editDVD.equals("price"))
+                			{
+                				String newPriceAsString = JOptionPane.showInputDialog("Please enter the new price:");
+                				double newPrice = Double.parseDouble(newPriceAsString);
+                    			// DVDArrayList.set(0,4);
+                    			JOptionPane.showMessageDialog (null, "The detail has been changed", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+                			}
+        } // end if
+        
+         if (menuName.equals("Remove DVD")) {
+          //customerArrayList.remove(customer);
+        } // end if
+        
+        	
+		if (menuName.equals("Exit System")) {
+			
+			
+			try{
+			String fileName = "customers.ser";
+				ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fileName));
+				os.writeObject(customerArrayList);
+				JOptionPane.showMessageDialog (null, "Wrote Customers", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+				os.close();
+			} catch (FileNotFoundException f) {
+				f.printStackTrace();
+			} catch (IOException f) {
+				f.printStackTrace();
+			}
+			
+			
+			try{
+			String fileName = "dvds.ser";
+				ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fileName));
+				os.writeObject(DVDArrayList);
+				JOptionPane.showMessageDialog (null, "Wrote DVDs", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
+				os.close();
+			} catch (FileNotFoundException f) {
+				f.printStackTrace();
+			} catch (IOException f) {
+				f.printStackTrace();
+			}		
+ 
+           System.exit(0);
+        } 		  				
+      } // end menu actionperformed
+      
+//private class RentButtonListener implements ActionListener {
+	 //public void actionPerformed(ActionEvent e)
+	  	//{
+	  		//String searchCustName = JOptionPane.showInputDialog("Please enter customer name to search for:");
+			//for (int i = 0; i<customerArrayList.size(); i=i+1){
 				
-			
-			
-			
-      } // end actionperformed
+				
+				
+			//}
+	  		
+	  	//}
+    //}// end buttonlistener inner class
+
+
         
-        
-        
-        //else if (menuName.equals("Add DVD")) {
-          	//title = JOptionPane.showInputDialog("Please enter the title of the DVD:");
-			//year = Integer.parseInt(JOptionPane.showInputDialog("Please enter the year the DVD was made:"));
-			//genre = JOptionPane.showInputDialog("Please enter the genre of the DVD:");
-			//rating = JOptionPane.showInputDialog("Please enter the rating of the DVD:");
-			//price = Float.parseFloat(JOptionPane.showInputDialog("Please enter the price of the DVD:"));
-			//JOptionPane.showMessageDialog (null, "The DVD has been added to the database.", "Confirmation Message", JOptionPane.INFORMATION_MESSAGE);
-        //} // end else if
-        //else if (menuName.equals("Edit DVD")) {
-           //welcomeLabel.setText("Menu Item '" + menuName + "' is selected.");
-        //} // end else if
-        //else if (menuName.equals("Remove DVD")) {
-           //welcomeLabel.setText("Menu Item '" + menuName + "' is selected.");
-        //} // end else if
-        //else
-        //{
-           //welcomeLabel.setText("Menu Item '" + menuName + "' is selected.");
-           //exit
-        
-           //System.exit(0);
-           //end else
-        //}	
 }
 
            
